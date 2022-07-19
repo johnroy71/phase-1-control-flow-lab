b1aedf618e4701e762bd022661694b5ef295f416
John Roy <johnroy71@gmail.com>
2022-07-18 21:06:46 -0500

    commit tree a3dd825797c698b7e8022afb6b00710f053d67dc

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+    // Write your code here!
+  if (someValue <= 400) return ("This one is on me!");
+  else if (someValue <=2000) return ("That will be twenty bucks.");
+  else if (someValue > 2000, someValue < 2500) return ("I will gladly take your thirty bucks.");
+  else if (someValue > 2500) return ("No can do.")  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someCity){
   // Write your code here!
+  const cityMessage = someCity === "NYC" ? "Ok, sounds good." : "No go.";
+  return cityMessage;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someTip){
   // Write your code here!
+if (someTip === "generous") return ("Thank you so much.");
+if (someTip === "not as generous") return ("Thank you.");
+else return ("Bye.");
 }
